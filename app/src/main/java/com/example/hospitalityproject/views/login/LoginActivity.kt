3731fca0d2ca231560b2abe44be5a9dab8bcc666
AraQuispe <mquispeyn@unsa.edu.pe>
2021-11-18 19:26:44 -0500

    Interfaz Log in y validación

--- app/src/main/java/com/example/hospitalityproject/views/login/LoginActivity.kt
+++ app/src/main/java/com/example/hospitalityproject/views/login/LoginActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.util.PatternsCompat
+import androidx.core.view.size
 import com.example.hospitalityproject.R
 import com.example.hospitalityproject.databinding.ActivityMainBinding
 import com.example.hospitalityproject.model.Initialization.Companion.pref
@@ -15,6 +16,7 @@ import com.example.hospitalityproject.views.MenuActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login2.*
 import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Pattern
 
 enum class ProviderType{
     BASIC
@@ -24,15 +26,20 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        //Validar
+        //buttonLogin.setOnClickListener {}
         setup()
     }
     private fun setup(){
         title="Hospitality"
         buttonLogin.setOnClickListener {
+            validate()
             if(editTextEmail.editText?.text.toString().isNotEmpty()&&editTextPassword.editText?.text.toString().isNotEmpty()){
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(editTextEmail.editText?.text.toString(),
                     editTextPassword.editText?.text.toString()).addOnCompleteListener {
                     Log.d("Test004", "$it")
+
+
                     if(it.isSuccessful){
                         val toast1 = Toast.makeText(
                             applicationContext,
@@ -60,19 +67,62 @@ class LoginActivity : AppCompatActivity() {
     private fun showAlert(){
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Se ha producido un errore de autenticacion de usuario");
+        builder.setMessage("Se ha producido un error de autenticacion de usuario");
         builder.setPositiveButton("Aceptar",null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
-    /*
+
+    //LLAMA MÉTODOS DE VALIDACIÓN
+    private fun validate(){
+        val result = arrayOf(validateEmail(), validatePassword())
+        if(false in result){
+            return
+        }
+    }
+    //VALIDAR EMAIL
     private fun validateEmail():Boolean{
         val email = editTextEmail.editText?.text.toString()
-        return  if(!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
-
+        return if (editTextEmail.editText?.text.toString().isEmpty()){
+            editTextEmail.error = "Este campo no puede ser vacío"
+            false
+        }else if(!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.error = "Ingrese un correo válido"
             false
         }else {
+            editTextEmail.error = null
             true
         }
-    }*/
+
+    }
+    //VALIDAR CONTRASEÑA
+    private fun validatePassword():Boolean{
+        val password = editTextPassword.editText?.text.toString()
+        val passwordRegex = Pattern.compile(
+            "^"+
+                    "(?=.*[0-9])"+          //Al menos un dígito
+                    "(?=.*[a-z])"+          //Al menos un caracter en minuscula
+                    "(?=.*[A-Z])"+          //Al menos un caracter en mayuscula
+                    "(?=.*[@#$%^&+=])"+     //Al menos un caracter especial
+                    "(?=\\S+$)"+            //Sin espacios
+                    ".{4,}"+                //al menos 4 caracteres
+                    "$"
+        )
+        return if (editTextPassword.editText?.text.toString().isEmpty()){
+            editTextPassword.error = "Este campo no puede ser vacío"
+            false
+        }else if(!passwordRegex.matcher(password).matches()){
+            /*Para registrar
+            if(editTextPassword.size<5){
+                editTextPassword.error = "Ingrese una contraseña más larga"
+            }else{
+                editTextPassword.error = "Ingrese otra contraseña"
+            }*/
+            editTextPassword.error = "Contraseña incorrecta"
+            false
+        }else {
+            editTextEmail.error = null
+            true
+        }
+    }
 }
