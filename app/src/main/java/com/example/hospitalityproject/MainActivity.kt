83ca2e93c3eae5c2b1a67a97c42a6253221377a7
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-12-18 22:14:53 -0500

    Primer commit categorías

--- app/src/main/java/com/example/hospitalityproject/MainActivity.kt
+++ app/src/main/java/com/example/hospitalityproject/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Hi", "Vale pruebas ")
+        Log.e("Hi", "Vale pruebas 2")
     }
 }
