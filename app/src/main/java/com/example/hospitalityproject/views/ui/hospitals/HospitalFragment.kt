d28296025606a3170cab984a7c6a9a88cbbb1064
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-12-18 23:36:38 -0500

    Carga Datos por Hospital

--- app/src/main/java/com/example/hospitalityproject/views/ui/hospitals/HospitalFragment.kt
+++ app/src/main/java/com/example/hospitalityproject/views/ui/hospitals/HospitalFragment.kt
@@ -109,7 +109,7 @@ class HospitalFragment : Fragment() {
             Log.e("InfoSpinner", spnCategoriasPorHospital.selectedItem.toString())
             Log.e("FechaEdt", edtSeleccFechaReserva.text.toString())
             Log.e("HoraReserva", inpSeleccHoraReserva.hour.toString() + ":" + inpSeleccHoraReserva.minute.toString())
-            Log.e("Pago", edtCodigoPagoReserva.text.toString())
+            Log.e("PagodeCita", edtCodigoPagoReserva.text.toString())
         }
 
         return view
